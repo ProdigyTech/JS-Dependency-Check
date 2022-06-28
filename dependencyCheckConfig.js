08df6aa5e6e087ca04f7b7db16d06c0b3ba66b5e
Javar Alexander <javar.alexander@gmail.com>
2022-06-28 02:04:01 -0400

    fix: ignore packages

--- dependencyCheckConfig.js
+++ dependencyCheckConfig.js
@@ -3,10 +3,22 @@ import { ciFailKeys, reportTypes } from "./bin/constants.js";
 export default process.env == "CI"
   ? {
       failOn: ciFailKeys.MAJOR,
-      ignorePackages: ["eslint", "prettier"],
+      ignorePackages: [
+        "eslint",
+        "prettier",
+        "eslint-plugin-jsx-a11y",
+        "eslint-plugin-prettier",
+        "@babel/preset-env"
+      ],
       reportType: reportTypes.CI,
     }
   : {
       failOn: ciFailKeys.MAJOR,
-      ignorePackages: ["eslint", "prettier"],
+      ignorePackages: [
+        "eslint",
+        "prettier",
+        "eslint-plugin-jsx-a11y",
+        "eslint-plugin-prettier",
+        "@babel/preset-env"
+      ],
     };
