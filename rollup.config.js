232aa3c769e385e4a521fb8c74db549e26dc0fb5
Javar Alexander <javar.alexander@gmail.com>
2022-06-28 02:14:46 -0400

    fix: update tests

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ export default {
   input: ["bin/index.js", "bin/constants.js"],
   plugins: [terser()],
   output: {
+    banner: "#!/usr/bin/env node",
     minifyInternalExports: true,
     minifyExternalImports: true,
     inlineDynamicImports: false,
