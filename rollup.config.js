0cb34f027c652b7b6a32293f9177bf03ea971b8e
Javar Alexander <javar.alexander@gmail.com>
2022-11-05 12:11:48 -0400

    chore: fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,15 @@
 // rollup.config.js
 import { terser } from "rollup-plugin-terser";
 
-
-export default [
-  {
-    input: "bin/index.js",
-    output: {
-      banner: "#!/usr/bin/env node",
-      minifyInternalExports: true,
-      inlineDynamicImports: false,
-      dir: "dist/",
-      format: "esm",
-    },
-    plugins: [terser()],
-  },
-  {
-    input: "bin/constants.js",
-    output: {
-      minifyInternalExports: false,
-      inlineDynamicImports: false,
-      dir: "dist/",
-      format: "esm",
-
-    },
-  },
-];
+export default {
+  input: ["bin/index.js"],
+  plugins: [terser()],
+  output: {
+    banner: "#!/usr/bin/env node",
+    minifyInternalExports: true,
+    minifyExternalImports: true,
+    inlineDynamicImports: true,
+    file: "dist/index.js",
+    format: "es"
+  }
+}
